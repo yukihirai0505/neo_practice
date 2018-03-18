bc2b4e959b5b4d28be69efcb1887e9be937d0d5d
Yuki Hirai <yukihirai0505@gmail.com>
2018-03-18 14:58:16 +0900

    Add neo practice code

---
+++ neo_practice/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+public class Class1 : SmartContract
+{
+    public static bool Main()
+    {
+        return true;
+    }
+}
